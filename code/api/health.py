f1cf67d7fe5eb15a9cc62cc8784129c81f6f2b85
mstoro <78480384+mstoro@users.noreply.github.com>
2021-09-15 10:31:50 +0300

    [CCTRI-3088] - Implement health endpoint (#2)
    * [CCTRI-3088] - Implement health endpoint
    
    * [CCTRI-3088] - update endpoint for health check

--- code/api/health.py
+++ code/api/health.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
-from api.utils import get_jwt, jsonify_data
+
+from api.client import SumoLogicCloudSIEMClient
+from api.utils import get_credentials, jsonify_data
 
 health_api = Blueprint('health', __name__)
 
 
 @health_api.route('/health', methods=['POST'])
 def health():
-    _ = get_jwt()
+    key = get_credentials()
+    client = SumoLogicCloudSIEMClient(key)
+    _ = client.health()
     return jsonify_data({'status': 'ok'})
--- code/tests/unit/conftest.py
+++ code/tests/unit/conftest.py
@@ -1,9 +1,10 @@
-import jwt
-
-from app import app
-from pytest import fixture
 from http import HTTPStatus
 from unittest.mock import MagicMock
+
+import jwt
+from pytest import fixture
+
+from app import app
 from api.errors import INVALID_ARGUMENT
 from tests.unit.payloads_for_tests import PRIVATE_KEY
 
@@ -21,7 +22,9 @@ def client():
 @fixture(scope='session')
 def valid_jwt(client):
     def _make_jwt(
-            key='some_key',
+            access_id='some_id',
+            access_key='some_key',
+            host='api.us2.sumologic.com',
             jwks_host='visibility.amp.cisco.com',
             aud='http://localhost',
             kid='02B1174234C29F8EFB69911438F597FF3FFEE6B7',
@@ -29,7 +32,9 @@ def valid_jwt(client):
             wrong_jwks_host=False
     ):
         payload = {
-            'key': key,
+            'access_id': access_id,
+            'access_key': access_key,
+            'host': host,
             'jwks_host': jwks_host,
             'aud': aud,
         }
@@ -38,7 +43,7 @@ def valid_jwt(client):
             payload.pop('jwks_host')
 
         if wrong_structure:
-            payload.pop('key')
+            payload.pop('access_key')
 
         return jwt.encode(
             payload, client.application.rsa_private_key, algorithm='RS256',
@@ -64,12 +69,54 @@ def invalid_json_expected_payload():
     return _make_message
 
 
-def mock_api_response(status_code=HTTPStatus.OK, payload=None):
+def mock_api_response(status_code=HTTPStatus.OK, payload=None, text=None):
     mock_response = MagicMock()
 
     mock_response.status_code = status_code
     mock_response.ok = status_code == HTTPStatus.OK
 
     mock_response.json = lambda: payload
+    mock_response.text = text
 
     return mock_response
+
+
+@fixture(scope='module')
+def ssl_error_expected_relay_response():
+    return {
+        'errors':
+            [
+                {
+                    'code': 'unknown',
+                    'message':
+                        'Unable to verify SSL certificate: '
+                        'Self signed certificate',
+                    'type': 'fatal'
+                }
+            ]
+    }
+
+
+@fixture
+def mock_exception_for_ssl_error():
+    mock_response = MagicMock()
+    mock_response.reason.args.__getitem__().verify_message = 'Self signed' \
+                                                             ' certificate'
+    return mock_response
+
+
+@fixture(scope='module')
+def connection_error_expected_relay_response():
+    return {
+        'errors':
+            [
+                {
+                    'code': 'connection error',
+                    'message':
+                        'Unable to connect to Sumo Logic Cloud SIEM, '
+                        'validate the configured API endpoint: '
+                        'https://api.us2.sumologic.com/api/sec/v1',
+                    'type': 'fatal'
+                }
+            ]
+    }
