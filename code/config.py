0f5be01a9e7d463abbc73b1e4ae68d57ed668ce3
Olena Shynkarenko <oshynk@softserveinc.com>
2021-10-07 18:37:57 +0300

    [CCTRI-3329] Tweak health endpoint

--- code/config.py
+++ code/config.py
@@ -7,4 +7,4 @@ class Config:
     USER_AGENT = ('SecureX Threat Response Integrations '
                   '<tr-integrations-support@cisco.com>')
     CTR_DEFAULT_ENTITIES_LIMIT = 100
-    CLOUD_SIEM_API_ENDPOINT = 'https://{host}/api/sec/v1'
+    CLOUD_SIEM_API_ENDPOINT = 'https://{host}/{api_path}'
